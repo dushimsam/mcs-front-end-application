e581876d2d9248278510dd3cfe46252de44f55e0
kelliaUmuhire <kellumuhire@gmail.com>
2021-07-28 22:24:27 +0200

    Fix contact part

--- pages/index.js
+++ pages/index.js
@@ -21,14 +21,47 @@ export default function Home() {
       <Events />
       <FAQ />
       <div className="mt-5 container-fluid bg-gray pt-5 pb-5">
-        <div className="d-flex flex-wrap">
-          <div>Get in Touch</div>
-          <div className="">
+        <div className="d-flex flex-wrap pb-5">
+          <div className="contact-part px-md-5 px-2 w-100 pt-md-5">
+            <h4>Get in Touch</h4>
+            <form className="mt-4 pb-5">
+              <div className="form-group">
+                <input
+                  type="text"
+                  className="form-control border-0"
+                  id="name"
+                  placeholder="Name"
+                />
+              </div>
+              <div className="form-group">
+                <input
+                  type="email"
+                  className="form-control border-0"
+                  id="email"
+                  aria-describedby="emailHelp"
+                  placeholder="Enter email"
+                />
+              </div>
+              <div className="form-group">
+                <textarea
+                  type="text"
+                  className="form-control border-0"
+                  id="message"
+                  placeholder="Message"
+                />
+              </div>
+
+              <button type="submit" className="btn green-btn rounded-pill px-3">
+                Send
+              </button>
+            </form>
+          </div>
+          <div className="pl-md-4">
             <Image
               src="/images/map.PNG"
               alt="Welcome picture"
               width={600}
-              height={500}
+              height={400}
             />
           </div>
         </div>
